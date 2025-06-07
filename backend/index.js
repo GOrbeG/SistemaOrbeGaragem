aa6d40ee229c1f693318da2a929aeff1c4b456e5
Luis Felipe <lipeepi80@gmail.com>
2025-06-07 11:12:04 -0300

    correção página login

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
