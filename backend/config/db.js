6748941cd05c1b1deba0437e0bd7fc1c71e33e01
Luis Felipe <lipeepi80@gmail.com>
2025-09-06 14:25:36 -0300

    login adm

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // --- INÍCIO DA CORREÇÃO ---
+  // Adicione este objeto para habilitar a conexão SSL exigida pelo Render
+  ssl: {
+    rejectUnauthorized: false
+  }
+  // --- FIM DA CORREÇÃO ---
 });
 
 pool.on('connect', () => {
